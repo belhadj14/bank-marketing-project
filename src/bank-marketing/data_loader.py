7028b735ba2653d08efdd9233c6636aa60b4225b
belhadj <belhadjt@gmail.com>
2025-01-14 16:23:12 +0100

    create explorer data

---
+++ src/bank-marketing/data_loader.py
@@ -0,0 +1,60 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "789d235c-bf36-446e-8ae6-b9d7b15d4c7e",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\"\"\"\n",
+    "   Data loading utility functionality\n",
+    "     TO-DO:\n",
+    "        -\n",
+    "\"\"\"\n",
+    "import os\n",
+    "import sys\n",
+    "import shutil\n",
+    "import time\n",
+    "\n",
+    "from pathlib import Path\n",
+    "\n",
+    "import requests\n",
+    "import io\n",
+    "import zipfile\n",
+    "\n",
+    "def download_data():\n",
+    "    pass\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "006470b6-8ba0-4be5-b98e-2ff78360f1f4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "dll",
+   "language": "python",
+   "name": "myenv"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
